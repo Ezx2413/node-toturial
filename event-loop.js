bcd3c33d4928d1feaa10bb23fde53fb77565b034
Ezx2413 <ahn2413.ahn@gmail.com>
2022-07-25 18:30:43 +0430

    thired change

---
+++ event-loop.js
@@ -0,0 +1,13 @@
+console.log('first');
+
+setTimeout(() => {
+    console.log('second');
+}, 0);
+
+console.log('third');
+
+console.log('fourth');
+
+console.log('fiveth');
+
+console.log('sixth');
