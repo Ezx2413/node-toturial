5baa69d3f94a4aba8fd8c19024f572472c72ff8d
Ezx2413 <ahn2413.ahn@gmail.com>
2022-07-27 02:56:33 +0430

    blog

---
+++ app.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+const express = require("express");
+const dotEnv = require("dotenv");
+const morgan = require("morgan");
+
+const indexRouter = require("./routes/index");
+
+//* Load Config
+dotEnv.config({ path: "./config/config.env" });
+
+const app = express();
+
+//* Logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+//* Tamplate Engine
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+//* Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//* Routes
+app.use(indexRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(
+  3000,
+  console.log(
+    `server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
